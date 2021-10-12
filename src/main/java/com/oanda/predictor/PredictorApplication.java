0bb7489f7c4b2f3b4014e803e674098fd1606156
Anatoly.Shkondin <anatoly.shkondin@nordigy.ru>
2021-10-13 00:50:46 +0300

    Kotlin 1.3

--- src/main/java/com/oanda/predictor/PredictorApplication.java
+++ src/main/java/com/oanda/predictor/PredictorApplication.java
@@ -1,7 +1,6 @@
 package com.oanda.predictor;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 @EnableScheduling
 @SpringBootApplication
-@EnableAutoConfiguration
 public class PredictorApplication {
 
     public static void main(String[] args) {
